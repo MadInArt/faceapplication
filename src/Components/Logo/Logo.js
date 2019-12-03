572105836eed69fef6c222dc1ead0e0b445050c4
MadInArt <knyazvladimir26@gmail.com>
2019-12-03 12:04:29 +0100

    uploading front

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tilt from 'react-tilt'
+import brain from './brain.png'
+const Logo = () => {
+	return(
+		<Tilt className="Tilt br2 shadow-2 ma3" options={{ max : 25 }} style={{ height: 120, width: 120 }} >
+			 <div className="Tilt-inner"><img style={{paddingTop:12}}  alt='brain' src ={brain}/></div>
+		</Tilt>
+		)
+}
+
+export default Logo;
