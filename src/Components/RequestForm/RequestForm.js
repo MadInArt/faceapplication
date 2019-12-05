f5f8932f301891208fbb44eba40fcfb1b5bc32f0
MadInArt <knyazvladimir26@gmail.com>
2019-12-05 15:34:56 +0100

    Update RequestForm.js

--- src/Components/RequestForm/RequestForm.js
+++ src/Components/RequestForm/RequestForm.js
@@ -4,7 +4,7 @@ import  './RequestForm.css';
 const RequestForm = ({onInputChange, onButtonClick}) => {
 	return(
 		<div>
-			<p>Give the link to an image, and this brain will magically detect faces on it</p>
+			<p>Give the link to an image, and this brain will magically detect face on it</p>
 		<div className='center pa4 br3 shadow-5'>
 			<input type='text' className='f4 pa2 w-70 center'onChange={onInputChange}/>
 			<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick ={onButtonClick}>Submit</button>
